4288d5b056147c932f1a8e7f1875493df8319f7c
Mooeantoal <147017270+Mooeantoal@users.noreply.github.com>
2025-09-03 13:18:56 +0800

    更新 danmaku_view.dart

--- lib/common/widget/danmaku_view.dart
+++ lib/common/widget/danmaku_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barrage/flutter_barrage.dart';
 
-class DanmakuView extends StatelessWidget {
+class DanmakuView extends StatefulWidget {
   final String? cid;
   final double fontSize;
   final double opacity;
@@ -16,16 +16,57 @@ class DanmakuView extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: BarrageView(
-          cid: cid,
-          fontSize: fontSize,
-          opacity: opacity,
-          showArea: showArea,
-        ),
+  State<DanmakuView> createState() => _DanmakuViewState();
+}
+
+class _DanmakuViewState extends State<DanmakuView> {
+  late BarrageWallController _barrageWallController;
+
+  @override
+  void initState() {
+    super.initState();
+    _barrageWallController = BarrageWallController();
+    // 这里可以先添加几条测试弹幕
+    Future.delayed(Duration(seconds: 1), () {
+      _barrageWallController.send([
+        Bullet(child: _buildText("欢迎进入弹幕区")),
+        Bullet(child: _buildText("CID: ${widget.cid ?? "未知"}")),
+      ]);
+    });
+  }
+
+  Text _buildText(String text) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: Colors.white.withOpacity(widget.opacity),
+        fontSize: widget.fontSize,
+        shadows: const [
+          Shadow(
+            blurRadius: 2,
+            color: Colors.black,
+            offset: Offset(1, 1),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.transparent, // 保持透明覆盖在播放器上
+      child: BarrageWall(
+        barrageWallController: _barrageWallController,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * widget.showArea,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _barrageWallController.dispose();
+    super.dispose();
   }
 }
