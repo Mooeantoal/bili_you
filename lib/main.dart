82932aa3383d09ea48ce96c1dd4b337c19906888
Mooeantoal <147017270+Mooeantoal@users.noreply.github.com>
2025-09-24 18:30:23 +0800

    更新 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:media_kit/media_kit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await BiliYouStorage.ensureInitialized();
+  await BiliMeStorage.ensureInitialized();
   MediaKit.ensureInitialized();
   runApp(const MyApp());
   //状态栏、导航栏沉浸
