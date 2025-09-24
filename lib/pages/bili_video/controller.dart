805b6a12ef5bcf1720038f34136cfbfddabb2418
Mooeantoal <147017270+Mooeantoal@users.noreply.github.com>
2025-09-24 09:54:19 +0800

    更新 controller.dart

--- lib/pages/bili_video/controller.dart
+++ lib/pages/bili_video/controller.dart
@@ -1,62 +1,130 @@
-import 'package:bili_you/pages/bili_video/widgets/bili_video_player/bili_danmaku.dart';
+import 'package:bili_you/common/api/video_info_api.dart';
+import 'package:bili_you/common/api/video_operation_api.dart';
+import 'package:bili_you/common/api/video_play_api.dart';
+import 'package:bili_you/common/models/local/video/video_info.dart';
+import 'package:bili_you/common/models/local/video/video_play_info.dart';
+import 'package:bili_you/common/utils/bili_you_storage.dart';
+import 'package:bili_you/common/utils/cache_util.dart';
+import 'package:bili_you/common/utils/settings.dart';
 import 'package:bili_you/pages/bili_video/widgets/bili_video_player/bili_video_player.dart';
 import 'package:bili_you/pages/bili_video/widgets/bili_video_player/bili_video_player_panel.dart';
-import 'package:bili_you/pages/bili_video/widgets/reply/index.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class BiliVideoController extends GetxController
-    with GetTickerProviderStateMixin {
+class BiliVideoController extends GetxController {
+  final String bvid;
+  final int cid;
+  final bool isBangumi;
+  final int? ssid;
+  final int? progress;
+
   BiliVideoController({
     required this.bvid,
     required this.cid,
+    this.isBangumi = false,
     this.ssid,
     this.progress,
-    required this.isBangumi,
   });
-  String bvid;
-  late String oldBvid;
-  int cid;
-  int? ssid;
-  int? progress;
-  bool isBangumi;
 
+  late VideoInfo videoInfo;
+  late VideoPlayInfo videoPlayInfo;
   late BiliVideoPlayerController biliVideoPlayerController;
-  late BiliVideoPlayerPanelController biliVideoPlayerPanelController;
-  late BiliDanmakuController biliDanmakuController;
-  late final TabController tabController;
-
-  Future<void> changeVideoPart(String bvid, int cid) async {
-    this.cid = cid;
-    this.bvid = bvid;
-    biliVideoPlayerController.bvid = bvid;
-    biliVideoPlayerController.cid = cid;
-    await biliVideoPlayerController.changeCid(bvid, cid);
-  }
-
-  refreshReply() {
-    Get.find<ReplyController>(tag: 'ReplyPage:$oldBvid').bvid = bvid;
-    Get.find<ReplyController>(tag: 'ReplyPage:$oldBvid')
-        .refreshController
-        .callRefresh();
-  }
+  late BiliVideoPlayerPanelController panelController;
+  final cacheManager = CacheUtils.biliYouCacheManager;
+  final isLoading = true.obs;
+  final isError = false.obs;
+  final errorMessage = "".obs;
+  final isLiked = false.obs;
+  final isCoined = false.obs;
+  final isFaved = false.obs;
+  final likeCount = 0.obs;
+  final coinCount = 0.obs;
+  final favCount = 0.obs;
+  final shareCount = 0.obs;
 
   @override
-  void onInit() {
-    oldBvid = bvid;
-    tabController = TabController(
-        length: 2,
-        vsync: this,
-        animationDuration: const Duration(milliseconds: 200));
-    biliVideoPlayerController = BiliVideoPlayerController(
-        bvid: bvid,
-        cid: cid,
-        initVideoPosition:
-            progress != null ? Duration(seconds: progress!) : Duration.zero);
-    biliVideoPlayerPanelController =
-        BiliVideoPlayerPanelController(biliVideoPlayerController);
-    biliDanmakuController = BiliDanmakuController(biliVideoPlayerController);
+  void onInit() async {
     super.onInit();
+    try {
+      await _loadVideoInfo();
+      await _loadVideoPlayInfo();
+      _initPlayer();
+      _initPanelController();
+      _loadInteractionState();
+      isLoading.value = false;
+    } catch (e) {
+      isLoading.value = false;
+      isError.value = true;
+      errorMessage.value = e.toString();
+      rethrow;
+    }
+  }
+
+  Future<void> _loadVideoInfo() async {
+    videoInfo = await VideoInfoApi.getVideoInfo(bvid: bvid);
+  }
+
+  Future<void> _loadVideoPlayInfo() async {
+    videoPlayInfo = await VideoPlayApi.getVideoPlayInfo(
+      bvid: bvid,
+      cid: cid,
+      isBangumi: isBangumi,
+      ssid: ssid,
+    );
+  }
+
+  void _initPlayer() {
+    biliVideoPlayerController = BiliVideoPlayerController(
+      videoPlayInfo: videoPlayInfo,
+      initialPosition: progress != null ? Duration(seconds: progress!) : null,
+    );
+  }
+
+  void _initPanelController() {
+    panelController = BiliVideoPlayerPanelController(
+      biliVideoPlayerController: biliVideoPlayerController,
+    );
+  }
+
+  Future<void> _loadInteractionState() async {
+    var state = await VideoOperationApi.getVideoInteractionState(bvid: bvid);
+    isLiked.value = state.isLiked;
+    isCoined.value = state.isCoined;
+    isFaved.value = state.isFaved;
+    likeCount.value = state.likeCount;
+    coinCount.value = state.coinCount;
+    favCount.value = state.favCount;
+    shareCount.value = state.shareCount;
+  }
+
+  Future<void> toggleLike() async {
+    var result = await VideoOperationApi.clickLike(
+      bvid: bvid,
+      isCancel: isLiked.value,
+    );
+    isLiked.value = !isLiked.value;
+    likeCount.value += isLiked.value ? 1 : -1;
+  }
+
+  Future<void> addCoin(int num) async {
+    var result = await VideoOperationApi.addCoin(
+      bvid: bvid,
+      num: num,
+    );
+    isCoined.value = true;
+    coinCount.value += num;
+  }
+
+  Future<void> toggleFav() async {
+    var result = await VideoOperationApi.toggleFav(
+      bvid: bvid,
+      isCancel: isFaved.value,
+    );
+    isFaved.value = !isFaved.value;
+    favCount.value += isFaved.value ? 1 : -1;
+  }
+
+  Future<void> shareVideo() async {
+    shareCount.value += 1;
   }
 
   @override
